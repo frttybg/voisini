d8d3728112adc5e1eb8f2978b44457d9ee933417
frttybg <ferit25a@gmail.com>
2026-08-22 03:28:27 +0200

    Bilesen yolu duzeltildi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import BottomNav from "@/components/BottomNav"; // Alt menüyü projeye dahil ediyoruz
+import BottomNav from "./components/BottomNav"; // Alt menüyü projeye dahil ediyoruz
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
