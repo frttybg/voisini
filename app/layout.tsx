50ae581dab8dda9571e33d3936f64b977e2ed874
frttybg <ferit25a@gmail.com>
2026-08-22 03:48:21 +0200

    Mobil alt navigasyon ve root layout optimizasyonu tamamlandi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import BottomNav from "./components/BottomNav"; // Alt menüyü projeye dahil ediyoruz
+import BottomNav from "./components/BottomNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,13 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased pb-16 md:pb-0`}
-      >
-        {/* Tüm sayfalar buraya yüklenir */}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased pb-16 md:pb-0`}>
         {children}
-
-        {/* Mobil cihazlarda her sayfada altta görünecek menü */}
         <BottomNav />
       </body>
     </html>
